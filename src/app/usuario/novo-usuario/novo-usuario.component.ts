f7af7ee877a40380997a19d74090b9521ee08d1d
viniciusSouza <vinicius.souza@estudante.ifms.edu.br>
2023-09-20 01:07:49 -0400

    mensagem de cpf invalido

--- src/app/usuario/novo-usuario/novo-usuario.component.ts
+++ src/app/usuario/novo-usuario/novo-usuario.component.ts
@@ -5,6 +5,9 @@ import { CpfService } from '../service/cpf.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Validacoes } from '../validacoes';
 import { Usuario } from '../usuario';
+import { MatDialog } from '@angular/material/dialog';
+import { Mensagem } from 'src/app/componentes/modal-mensagem/mensagem';
+import { ModalMensagemComponent } from 'src/app/componentes/modal-mensagem/modal-mensagem.component';
 
 @Component({
   selector: 'app-novo-usuario',
@@ -20,26 +23,25 @@ export class NovoUsuarioComponent implements OnInit {
     private service: UsuarioService,
     private cpfService: CpfService,
     private rota: Router,
-    private router: ActivatedRoute
+    private router: ActivatedRoute,
+    private dialog: MatDialog
   ) {}
 
   ngOnInit(): void {
-    this.usuarioForm = this.formBuilder.group(
-      {
-        nome: ['', [Validators.required, Validators.minLength(4)]],
-        cpf: [
-          '',
-          [Validacoes.ValidaCpf, Validators.required],
-          [this.cpfService.cpfCadastrado()],
-        ],
-        dataNascimento: ['', Validators.required],
-        rg: [''],
-        mae: [''],
-        pai: [''],
-        senha: ['', [Validators.required, Validators.minLength(7)]],
-        senha1: [''],
-      }
-    );
+    this.usuarioForm = this.formBuilder.group({
+      nome: ['', [Validators.required, Validators.minLength(7)]],
+      cpf: [
+        '',
+        [Validacoes.ValidaCpf, Validators.required],
+        [this.cpfService.cpfCadastrado()],
+      ],
+      dataNascimento: ['', Validators.required],
+      rg: [''],
+      mae: [''],
+      pai: [''],
+      senha: ['', [Validators.required, Validators.minLength(7)]],
+      senha1: [''],
+    });
   }
 
   cadastrar() {
@@ -51,9 +53,24 @@ export class NovoUsuarioComponent implements OnInit {
     if (this.usuarioForm.valid) {
       const novo = this.usuarioForm.getRawValue() as Usuario;
 
-      this.service.salvarUsuario(novo, id).subscribe(() => {
-        this.rota.navigate(['home']);
-      });
+      this.service.salvarUsuario(novo, id).subscribe(
+        () => {
+          this.rota.navigate(['home']);
+        },
+        (error) => {
+          if (error.status == '403') {
+            this.dialog.open(
+              ModalMensagemComponent
+            ).componentInstance.msgModal = this.mensagem;
+          }
+        }
+      );
     }
   }
+
+  mensagem: Mensagem = {
+    titulo: 'CPF Inválido',
+    texto:
+      'O número do CPF está incorreto, verifique o número inserido e tente novamente',
+  };
 }
